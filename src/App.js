7d784f7843e05ba04039c200dbb405232251ef48
Sanjeeva <sanjeevphns@gmail.com>
2022-09-19 20:06:32 +0530

    added edit function updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import { useContext} from 'react';
+import { useContext, useState} from 'react';
 import './App.css';
 import { Context } from './Context';
 
 
 const App = () => {
 
-    // const [value, setValue] = useState('');
-    // const [tasks, setTasks] = useState([]);
-    // let [count, setCount] = useState(0);
-    // const [deletedTasks, setDeletedTasks] = useState([]);
     
     const {value, setValue, tasks, setTasks, count, setCount, deletedTasks, setDeletedTasks} = 
     useContext(Context);
+    const [id, setId] = useState(null);
+    const [editOn, setEditOn] = useState(false);
+    const [current, setCurrent] = useState('');
 
     
     const handleSubmit = (e)=>{
@@ -19,10 +18,14 @@ const App = () => {
         setTasks(prevState => [...prevState, {'task':value, 'id':count}]);
         e.preventDefault();
         setValue('');
+        setEditOn(false);
     }
 
-    const handleDelete = (id)=>{
+    const handleDelete = (id, task)=>{
 
+    if (current === task){
+      setEditOn(false);
+    }
     const delTask = tasks.filter(ele => {
       return ele.id === id;
     })
@@ -37,8 +40,46 @@ const App = () => {
     setTasks(newTask);
   }
 
+  const handleEdit = (id, task)=>{
+    
+    setId(id);
+    setCurrent(task);
+    setEditOn(true);
+    console.log(id, task, editOn);
+  }
+  
+  const handleSave = (e)=>{
+    if(current !== ''){
+      const newTask = tasks.filter(ele =>{
+        return id !== ele.id;
+      });
+      setTasks(newTask);
+      setTasks(prevState => [...prevState, {'task':current, 'id':id}])
+      setEditOn(false)
+    }
+    e.preventDefault();
+  }
+
+  const editForm = <div className='editForm'>
+  <h2>Edit Task</h2>
+    <form onSubmit={handleSave} >
+    <input 
+    className='editForm-entry'
+    type='text'
+    value = {current}
+    onChange = {e => setCurrent(e.target.value)}
+    />
+    <input 
+      className='editForm-submit'
+      type='submit'
+      value='Update'
+    />
+  </form>
+  </div>
+
   return (
     <div>
+        <div className='mainSection'>
         <form onSubmit={handleSubmit} className='inputForm'>
         <input
         className='inputForm-entry'
@@ -62,15 +103,18 @@ const App = () => {
             {task}
             </div>
             <div className='buttons'>
+            <button className='editButton' 
+            onClick={()=>handleEdit(id, task)}>Edit</button>
             <button className='deletButton' 
-            onClick={()=>handleDelete(id)}>Delete</button>
-            <button className='deletButton' 
-            onClick={()=>handleDelete(id)}>Delete</button>
+            onClick={()=>handleDelete(id, task)}>Delete</button>
             </div>
             </div>
             ))}
             </ul>
     </div>
+        </div>
+
+    {editOn ? editForm : ''}
    
     </div>
   )
